607305504d988d36d998a6e85815386b8272624c
pokornap <58918202+pokornap@users.noreply.github.com>
2020-01-05 21:31:03 -0500

    Started Movie Vue Project. made a Movie component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Movie MovieTitle="Avengers: Endgame" MovieDescription="This is an amazing movie, go watch it!" ImageLoc="AvengersEndgame.png"/>
+    <Movie v-for="movie in movies" MovieTitle=`${movie.title}`/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Movie from './components/Movie.vue'
 export default {
   name: 'app',
+  data: () => {
+    movies: [
+      {title: "Avengers: Endgame", description: "This is an amazing movie, go watch it!", image: "logo.png"},
+      {title: "Star Wars", description: "The OG Star Wars", image: "logo.png"},
+      {title: "Atomic Blonde", description: "The CIA always wins", image: "logo.png"}
+    ]
+  },
   components: {
-    HelloWorld
+    Movie
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Lucida Console", Monaco, monospace;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Movie.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="Movie">
+        <img alt="Avengers Poster" :src="require(`@/assets/${ImageLoc}`)"/>
+        <h1>{{MovieTitle}}</h1>
+        <p class="description">
+            {{MovieDescription}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Movie",
+    props: {
+        MovieTitle: String,
+        MovieDescription: String,
+        ImageLoc: String
+    }
+}
+</script>
+
+<style scoped>
+.Movie {
+    background-color: blue;
+    position: relative;
+    display: float;
+    float: left;
+    
+}
+img {
+    max-width: 400px;
+    max-height: 400px;
+}
+</style>
