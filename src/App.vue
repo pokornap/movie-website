c2fd8bb4343f13b7ca17dfdd52f3f4bcc3b45a3f
pokornap <58918202+pokornap@users.noreply.github.com>
2020-01-05 22:39:39 -0500

    nbkkillaJ: allowed for dynamic building of movies

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,56 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Movie MovieTitle="Avengers: Endgame" MovieDescription="This is an amazing movie, go watch it!" ImageLoc="AvengersEndgame.png"/>
-    <Movie v-for="movie in movies" MovieTitle=`${movie.title}`/>
+    <div v-for="(movie, index) in movies" :key="index">
+      <Movie
+      :MovieTitle="movie.title"
+      :MovieDescription="movie.description"
+      :ImageLoc="movie.image"
+    />
+    </div>
+    
   </div>
 </template>
 
 <script>
-import Movie from './components/Movie.vue'
+import Movie from "./components/Movie.vue";
 export default {
-  name: 'app',
-  data: () => {
-    movies: [
-      {title: "Avengers: Endgame", description: "This is an amazing movie, go watch it!", image: "logo.png"},
-      {title: "Star Wars", description: "The OG Star Wars", image: "logo.png"},
-      {title: "Atomic Blonde", description: "The CIA always wins", image: "logo.png"}
-    ]
-  },
+  name: "app",
+  data: () => ({
+      movies: [
+        {
+          title: "Avengers: Endgame",
+          description: "This is an amazing movie, go watch it!",
+          image: "AvengersEndgame.png"
+        },
+        {
+          title: "Star Wars",
+          description: "The OG Star Wars",
+          image: "StarWars.png"
+        },
+        {
+          title: "Atomic Blonde",
+          description: "The CIA always wins",
+          image: "AtomicBlonde.png"
+        }
+      ]
+  }),
   components: {
     Movie
   }
-}
+};
 </script>
 
 <style>
+html body {
+  background-color: #2c3e50;
+}
 #app {
   font-family: "Lucida Console", Monaco, monospace;
   font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -21,14 +21,18 @@ export default {
 
 <style scoped>
 .Movie {
-    background-color: blue;
+    background-color: #2c2c50;
+    color: white;
     position: relative;
     display: float;
     float: left;
-    
+    border-radius: 25px;
+    margin: 10px
 }
 img {
     max-width: 400px;
+    /* min-width: 400px; */
     max-height: 400px;
+    /* min-height: 400px; */
 }
 </style>
