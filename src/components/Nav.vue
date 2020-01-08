e2c7dbc1493c44a717b3d6aeeff6d88a30a61a29
pokornap <58918202+pokornap@users.noreply.github.com>
2020-01-07 23:35:23 -0500

    added a background color to the div

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,23 @@
 <template>
-    
+    <div class="nav">
+        <button>Add Movie</button>
+    </div>    
 </template>
 
 <script >
 export default {
-    
+    name: "nav",
+    props: 
+
+
 }
 </script>
 
 <style scoped>
 
+.nav {
+    background-color: #2c3e50
+}
 
 
 
